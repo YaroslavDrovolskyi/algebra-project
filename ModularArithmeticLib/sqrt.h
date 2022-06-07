e5f52fc71054160dbaa623c7a4516c29d830cc34
MrChickenator <andreych237@gmail.com>
2022-06-08 01:50:01 +0300

    public

--- ModularArithmeticLib/sqrt.h
+++ ModularArithmeticLib/sqrt.h
@@ -6,11 +6,11 @@
 class Sqrt
 {
 private:
-	static int powerMod(int base, int exponent, int modulus);
 	static int gcd(int a, int b);
 	static int orderValues(int p, int b);
 	static int findx2e(int x, int& e);
 
 public:
-	int calcSquareRoot(int n, int p);
+	static int powerMod(int base, int exponent, int modulus);
+	static int calcSquareRoot(int n, int p);
 };

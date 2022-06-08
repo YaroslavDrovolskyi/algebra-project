bec25775a3df097d4848a3206f8794cb0192e899
Anton Kachanovski <91565960+no1zyx@users.noreply.github.com>
2022-06-08 09:15:51 +0300

    Add files via upload
    рекурсивна функція алгоритму Евкліда для пошуку оберненого багаточлену

---
+++ ModularArithmeticLib/EuclideanAlgorithm.h
@@ -0,0 +1,17 @@
+#ifndef EUCLIDEANALGORITHM_H
+#define EUCLIDEANALGORITHM_H
+#include <iostream>
+
+#include <cmath>
+#include "ModularArithmeticLib_global.h"
+
+
+
+
+MODULARARITHMETICLIB_EXPORT int gcd(int a,int b, int& x,int& y); //рекурсивна функція алгоритму Евкліда для пошуку оберненого багаточлену
+
+
+
+
+
+#endif //ALGEBRA_TASK5_EUCLIDEAN_ALGORITHM
